b2fd7d42b98ef272e7c7fcd6ca279ada508c653f
jkehl2 <kehl.pro@gmail.com>
2020-11-09 14:20:51 +0100

    CREATION TABLE AVEC SEQUELIZE :)

--- models/index.js
+++ models/index.js
@@ -2,28 +2,39 @@ const Card = require('./card');
 const List = require('./list');
 const Tag = require('./tag');
 const CardHasTag = require('./cardHasTag');
+const sequelize = require('sequelize');
 
-List.hasMany(Card,{
-    foreignKey:'listId',
+List.hasMany(Card, {
+    foreignKey: 'listId',
     as: 'cards'
 });
 
-Card.hasOne(List,{
-    foreignKey:'listId',
+Card.hasOne(List, {
+    foreignKey: 'listId',
     as: 'list'
 })
 
-Tag.belongsToMany(Card,{
+Tag.belongsToMany(Card, {
     foreignKey: 'tagId',
-    otherKey:'cardId',
-    as:'cards',
-    through:CardHasTag
+    otherKey: 'cardId',
+    as: 'cards',
+    through: CardHasTag
 });
 
-Card.belongsToMany(Tag,{
+Card.belongsToMany(Tag, {
     foreignKey: 'cardId',
-    otherKey:'tagId',
-    as:'tags',
-    through:CardHasTag
+    otherKey: 'tagId',
+    as: 'tags',
+    through: CardHasTag
 });
-module.exports = {Card, List, Tag, CardHasTag};
+
+sequelize.sync({
+    force: true
+});
+
+module.exports = {
+    Card,
+    List,
+    Tag,
+    CardHasTag
+};
