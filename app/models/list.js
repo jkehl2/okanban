756e8cbbe6fd9a8875b98df9bafafce548a71038
SimonMARTIN87 <simon@oclock.io>
2020-11-12 11:55:29 +0100

    listController is quite ready

--- app/models/list.js
+++ app/models/list.js
@@ -7,7 +7,17 @@ class List extends Model {
 
 
 List.init({
-    name: DataTypes.TEXT,
+    // name: DataTypes.TEXT,  // première version => trop simple !
+    // on peut définir les propriétés de manière plus fine
+    // et ainsi traiter un paquet d'erreurs, sans rien faire !
+    // cf : //https://sequelize.org/master/manual/validations-and-constraints.html
+    name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }        
+    },
     position: DataTypes.INTEGER
 }, {
     // le nom de la table
--- app/router.js
+++ app/router.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const listController = require('./controllers/listController');
 
 const router = express.Router();
 
+/** CRUD List */
+router.get('/list', listController.getAll );
+router.get('/list/:id', listController.getOne );
+router.post('/list', listController.create );
+router.patch('/list', listController.updateAll );
+router.patch('/list/:id', listController.updateOne );
+router.delete('/list/:id', listController.deleteOne );
 
-
+/** middleware 404, comme d'habitude toujours en dernier ! */
+router.use( (req,res) => {
+    res.status(404).json( {error: "not found"} );
+});
 
 module.exports = router;
