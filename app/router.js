4120d588a55ef104ca3b5d5868d0ede3be24863a
SimonMARTIN87 <simon@oclock.io>
2020-11-12 12:14:24 +0100

    syntaxe alternative router

--- app/router.js
+++ app/router.js
@@ -11,6 +11,20 @@ router.patch('/list', listController.updateAll );
 router.patch('/list/:id', listController.updateOne );
 router.delete('/list/:id', listController.deleteOne );
 
+/** Syntaxe alternative */
+/*
+router.route('/list')
+    .get( listController.getAll )
+    .post( listController.create )
+    .patch( listController.updateAll );
+
+router.route('/list/:id')
+    .get( listController.getOne )
+    .patch( listController.updateOne )
+    .delete( listController.deleteOne );
+*/
+
+
 /** middleware 404, comme d'habitude toujours en dernier ! */
 router.use( (req,res) => {
     res.status(404).json( {error: "not found"} );
